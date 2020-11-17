56f2fb735201ac0796bf2f9c1a048ba52b1a8a4e
Etienne Burdet <etienne.burdet@gmail.com>
2020-11-17 18:21:54 +0100

    Get features in console

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,55 @@
 <script>
-	export let name;
+  import { onMount, setContext } from 'svelte'
+  import mapbox from 'mapbox-gl'
+
+  import ClusterLayer from './components/ClusterLayer.svelte'
+	import Marker from './components/Marker.svelte'
+
+  mapbox.accessToken = 'pk.eyJ1IjoiZXRpZW5uZWJ1cmRldCIsImEiOiJja2F4bHN2MXUwMGliMnJsN2RzNXowYzQ1In0.ju6w4WN1F_CRVNXtp5L-7w';
+
+  let map
+  let container
+  let features = []
+
+	const setFeatures = (map) => () => {
+		features = map.querySourceFeatures('earthquakes')
+	}
+	const updateFeatures = (map) => () => {
+		map.on('move', setFeatures(map))
+		map.on('moveend', setFeatures(map))
+		setFeatures(map)
+	}
+
+  setContext('mapbox', {
+    mapbox: mapbox,
+    getMap: () => map,
+  })
+
+  onMount(() => {
+      map = new mapbox.Map({
+      container,
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-79.381000, 43.646000],
+      zoom: 12.0
+    })
+
+		map.on('data', updateFeatures(map))
+  })
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div bind:this={container}>
+	{#if map}
+	<ClusterLayer />
+		{#each features as feature}
+			{console.log(feature)}
+		{/each}
+	{/if}
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  div {
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+  }
 </style>
---
+++ src/components/Marker.svelte
@@ -0,0 +1,14 @@
+<script>
+import { getContext, onMount } from 'svelte'
+
+const { mapbox, getMap } = getContext('mapbox')
+const map = getMap()
+
+export let feature
+</script>
+
+<div>
+  <svg width="">
+
+  </svg>
+</div>
